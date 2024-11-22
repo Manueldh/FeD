b99cc89dcff74ac2459fcb6c35c69e82bfe1fa23
Manuel den Hartog <145329293+Manueldh@users.noreply.github.com>
2024-11-22 14:49:06 +0100

    ja

--- scripts/script (Conflicted copy from Manuel’s MacBook Pro on 2024-11-22).js
+++ scripts/script (Conflicted copy from Manuel’s MacBook Pro on 2024-11-22).js
@@ -1,8 +1,6 @@
 const burgerBtn = document.querySelector("header button:nth-of-type(2)")
 const closeBtn = document.querySelector("header button:nth-of-type(1)")
 const headerNav = document.querySelector("header nav")
-const firstNavBtn = document.querySelector("#firstBtn")
-const secondNavBtn = document.querySelector("#secondBtn")
 
 burgerBtn.addEventListener("click", openMenu)
 closeBtn.addEventListener("click", closeMenu)
@@ -10,15 +8,9 @@ closeBtn.addEventListener("click", closeMenu)
 function openMenu() {
     headerNav.classList.add("toonMenu")
     burgerBtn.classList.add("removeBtn")
-    firstNavBtn.classList.add("showBtn")
-    secondNavBtn.classList.add("showBtn")
-    closeBtn.classList.remove("removeBtn")
 }
 
 function closeMenu() {
     headerNav.classList.remove("toonMenu")
-    burgerBtn.classList.remove("removeBtn")
-    closeBtn.classList.add("removeBtn")
-    firstNavBtn.classList.remove("showBtn")
-    secondNavBtn.classList.remove("showBtn")
+     burgerBtn.classList.remove("removeBtn")
 }
